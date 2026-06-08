53f2ba0c6fa4f57ca570fd63204de1fa796dfa67
Felipe-Urrutia <felipe.urrutia.sal@gmail.com>
2026-06-08 00:34:40 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'scanner-pro-v10';
+const CACHE_NAME = 'scanner-pro-v11';
 
 // Archivos críticos que se guardarán para uso offline inmediato
 // (Ahora utilizando rutas relativas para evitar errores de despliegue)
