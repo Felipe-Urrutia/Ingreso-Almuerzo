0d0ab0680bc2fcacc8a9e58787b4ff528113d064
Felipe-Urrutia <felipe.urrutia.sal@gmail.com>
2026-06-11 14:25:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 // Archivos críticos que se guardarán para uso offline inmediato
 // (Ahora utilizando rutas relativas para evitar errores de despliegue)
-const CACHE_NAME = 'scanner-pro-v24'; 
+const CACHE_NAME = 'scanner-pro-v25'; 
 
 const ASSETS_TO_CACHE = [                 // 👈 Raíz absoluta del servidor local
   './',                  // 👈 Raíz relativa
